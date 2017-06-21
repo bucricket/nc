a5bb887fac703a8ecacae5fe760dee100d778bab
bucricket <bucricket@gmail.com>
2017-06-21 09:56:11 -0400

    Removed an error statement that was stopping the code.

--- nc/noaaclass.py
+++ nc/noaaclass.py
@@ -182,9 +182,11 @@ class Connection(object):
     def last_response(self, response):
         packed = self.pack(response).select('h1')
         print(packed[0].text)
-        if (response.status_code != requests.codes.ok or
-            (packed and 'An Error Occurred' in packed[0].text)):
+        if (response.status_code != requests.codes.ok):
             raise Exception('Connection error (%i).' % response.status_code)
+#        if (response.status_code != requests.codes.ok or
+#            (packed and 'An Error Occurred' in packed[0].text)):
+#            raise Exception('Connection error (%i).' % response.status_code)
         self._last_response = response
 
     @property
